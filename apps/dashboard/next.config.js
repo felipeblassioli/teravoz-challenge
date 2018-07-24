25634729a65bc123eb26722b3cd1c82e993a5fce
Felipe Blassioli <felipeblassioli@gmail.com>
2018-07-24 06:22:23 -0400

    Config from environment

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,7 +1,7 @@
-const withCss = require('@zeit/next-css')
+const withCss = require('@zeit/next-css');
 
-module.exports = withCss({
-  webpack (config) {
+const webpackConfig = {
+  webpack(config) {
     config.module.rules.push({
       test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
       use: {
@@ -10,11 +10,35 @@ module.exports = withCss({
           limit: 100000,
           publicPath: './',
           outputPath: 'static/',
-          name: '[name].[ext]'
-        }
-      }
-    })
+          name: '[name].[ext]',
+        },
+      },
+    });
 
-    return config
+    return config;
+  },
+};
+
+function ensureValidUrl(variableName, url) {
+  if (!url) {
+    throw new Error(`Missing environment variable ${variableName}`);
   }
-})
+  // A TypeError will be thrown if the input or base are not valid URLs.
+  // eslint-disable-next-line no-new
+  new URL(url);
+}
+const teravozServiceUrl = process.env.TERAVOZ_SERVICE_URL;
+ensureValidUrl('TERAVOZ_SERVICE_URL', teravozServiceUrl);
+
+const apiServiceUrl = process.env.API_SERVICE_URL;
+ensureValidUrl('API_SERVICE_URL', apiServiceUrl);
+
+const config = {
+  ...webpackConfig,
+  publicRuntimeConfig: {
+    teravozServiceUrl,
+    apiServiceUrl,
+  },
+};
+
+module.exports = withCss(config);
